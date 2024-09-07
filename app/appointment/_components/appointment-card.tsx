e8451ebb12561fda7dc73b633a44a2fcc1dc1625
nerwinalamas <nerwinalamas@gmail.com>
2024-09-07 14:48:37 +0800

    feat: appointment card

---
+++ app/appointment/_components/appointment-card.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { AppointmentProps } from "../_types";
+import { formatDate } from "../_lib/utils";
+
+const AppointmentCard = ({ id, date, available_slots }: AppointmentProps) => {
+    const { dayName, formattedDate } = formatDate(date);
+
+    return (
+        <Link
+            href={`/appointment/${id}`}
+            className="flex flex-col justify-between h-40 p-4 rounded-lg bg-slate-100 text-slate-800"
+        >
+            <div className="flex flex-col gap-1">
+                <h1 className="text-xl font-semibold uppercase">{dayName}</h1>
+                <h2 className="text-sm font-bold">{formattedDate}</h2>
+            </div>
+            {available_slots === 0 ? (
+                <p className="text-sm uppercase">Fully Booked</p>
+            ) : (
+                <p className="text-sm uppercase">
+                    {available_slots} Slots Available
+                </p>
+            )}
+        </Link>
+    );
+};
+
+export default AppointmentCard;
