fced8e06fff9c335cc480645c8500661fd514d0d
nerwinalamas <nerwinalamas@gmail.com>
2024-09-08 20:12:26 +0800

    refactor: card if is booked or no

--- app/appointment/[appointmentDateId]/_components/time-card.tsx
+++ app/appointment/[appointmentDateId]/_components/time-card.tsx
@@ -1,22 +1,31 @@
-"use client"
+"use client";
 
 import { useParams } from "next/navigation";
 import { formatTime } from "../_lib/utils";
 import { Slot } from "../_types";
 import Link from "next/link";
 
-const TimeCard = ({
-    id,
-    start_time,
-    end_time
-}: Slot) => {
+const TimeCard = ({ id, start_time, end_time, is_booked }: Slot) => {
     const params = useParams();
 
     const formattedStartTime = formatTime(start_time);
     const formattedEndTime = formatTime(end_time);
 
+    if (is_booked) {
+        return (
+            <div className="flex items-center justify-center h-40 p-4 rounded-lg cursor-not-allowed bg-red-400 text-slate-800">
+                <div className="text-xl font-bold">
+                    {formattedStartTime} - {formattedEndTime}
+                </div>
+            </div>
+        );
+    }
+
     return (
-        <Link href={`/appointment/${params.appointmentDateId}/${id}`} className="flex items-center justify-center h-40 p-4 rounded-lg bg-slate-100 text-slate-800">
+        <Link
+            href={`/appointment/${params.appointmentDateId}/${id}`}
+            className="flex items-center justify-center h-40 p-4 rounded-lg bg-green-400 text-slate-800"
+        >
             <div className="text-xl font-bold">
                 {formattedStartTime} - {formattedEndTime}
             </div>
