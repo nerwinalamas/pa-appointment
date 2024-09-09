ed6a515d8c0c42018b81b1aa706639399b1dc4d0
nerwinalamas <nerwinalamas@gmail.com>
2024-09-09 11:35:30 +0800

    refactor: time card

--- app/appointment/[appointmentDateId]/_components/time-card.tsx
+++ app/appointment/[appointmentDateId]/_components/time-card.tsx
@@ -1,13 +1,14 @@
-"use client";
-
-import { useParams } from "next/navigation";
 import { formatTime } from "../_lib/utils";
 import { Slot } from "../_types";
 import Link from "next/link";
 
-const TimeCard = ({ id, start_time, end_time, is_booked }: Slot) => {
-    const params = useParams();
-
+const TimeCard = ({
+    id,
+    start_time,
+    end_time,
+    is_booked,
+    appointmentDateId,
+}: Slot & { appointmentDateId: string }) => {
     const formattedStartTime = formatTime(start_time);
     const formattedEndTime = formatTime(end_time);
 
@@ -23,7 +24,7 @@ const TimeCard = ({ id, start_time, end_time, is_booked }: Slot) => {
 
     return (
         <Link
-            href={`/appointment/${params.appointmentDateId}/${id}`}
+            href={`/appointment/${appointmentDateId}/${id}`}
             className="flex items-center justify-center h-40 p-4 rounded-lg bg-green-400 text-slate-800"
         >
             <div className="text-xl font-bold">
