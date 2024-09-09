142fc3bbf99dba1f64bc0e09c68fb16459513a17
nerwinalamas <nerwinalamas@gmail.com>
2024-09-09 11:33:26 +0800

    feat: payment heading component

---
+++ app/appointment/[appointmentDateId]/[appointmentTimeId]/_components/payment-heading.tsx
@@ -0,0 +1,29 @@
+import { formatDate } from "@/app/appointment/_lib/utils";
+import { formatTime } from "../../_lib/utils";
+import { getAppointmentSchedule } from "../service";
+
+const PaymentHeading = async ({
+    dateId,
+    timeId,
+}: {
+    dateId: string;
+    timeId: string;
+}) => {
+    const { appointment, slot } = await getAppointmentSchedule(dateId, timeId);
+    const { dayName, formattedDate } = formatDate(appointment.date);
+    const formattedStartTime = formatTime(slot.start_time);
+    const formattedEndTime = formatTime(slot.end_time);
+
+    return (
+        <h1 className="mb-5 text-2xl text-center md:w-[70%] md:mb-7 lg:w-[60%]">
+            To reserve your slot on{" "}
+            <span className="font-semibold">{formattedDate}</span>, from{" "}
+            <span className="font-semibold">
+                {formattedStartTime} to {formattedEndTime} ({dayName})
+            </span>
+            , kindly pay the required deposit.
+        </h1>
+    );
+};
+
+export default PaymentHeading;
