6853d000a3c4d0957759e22a4cf0b53e813c1ddd
nerwinalamas <nerwinalamas@gmail.com>
2024-09-09 11:31:40 +0800

    refactor: get params in server component

--- app/appointment/[appointmentDateId]/page.tsx
+++ app/appointment/[appointmentDateId]/page.tsx
@@ -1,26 +1,22 @@
-import { createClient } from "@/utils/supabase/server";
 import TimeCard from "./_components/time-card";
+import { getAllSlots } from "./service";
 
-//kailangan ang makukuha lang is yung galing sa appointment date id hindi lahat
-// bali dapat ganito
-// select * from slots where id = appointmentDateId
-
-const SelectTime = async () => {
-    const supabase = await createClient();
-    const { data, error } = await supabase.from("slots").select("*");
+const SelectTime = async ({ params }: { params: { appointmentDateId: string } }) => {
+    const { data, error } = await getAllSlots(params.appointmentDateId);
 
     if (error) {
-        return <div>Error: Hindi makuha ang data ng oras</div>;
+        return <div>Error: {error}</div>;
     }
 
     return (
         <div className="max-w-screen-xl mx-auto p-4 grid grid-cols-2 gap-4 md:grid-cols-3 lg:w-[80%] xl:grid-cols-6">
-            {data.map((time) => (
+            {data?.map((time) => (
                 <TimeCard
                     key={time.id}
                     startTime={time.start_time.slice(0, 5)}
                     endTime={time.end_time.slice(0, 5)}
                     {...time}
+                    appointmentDateId={params.appointmentDateId}
                 />
             ))}
         </div>
