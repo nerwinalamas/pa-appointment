ff1ee9a0ef8fdf1c3d93fd24e9f689e2292d0b1a
nerwinalamas <nerwinalamas@gmail.com>
2024-09-10 10:08:41 +0800

    feat: hook for slot modal

---
+++ hooks/useSlotModal.tsx
@@ -0,0 +1,20 @@
+import { Slot } from "@/app/appointment/[appointmentDateId]/_types";
+import { create } from "zustand";
+
+export type ModalType = "createSlot" | "updateSlot";
+
+type ModalStore = {
+    type: ModalType | null;
+    data?: Slot | string;
+    isOpen: boolean;
+    onOpen: (type: ModalType, data?: Slot | string) => void;
+    onClose: () => void;
+};
+
+export const useSlotModal = create<ModalStore>((set) => ({
+    type: null,
+    dataObject: {},
+    isOpen: false,
+    onOpen: (type, data) => set({ isOpen: true, type, data }),
+    onClose: () => set({ type: null, isOpen: false }),
+}));
