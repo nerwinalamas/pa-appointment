9962bd2daf4fed4035c5cdcbf6cfae8e2aa75634
nerwinalamas <nerwinalamas@gmail.com>
2024-09-10 10:09:20 +0800

    feat: create appointment modal

---
+++ components/modals/CreateAppointment.tsx
@@ -0,0 +1,103 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
+import { FormEvent, useState } from "react";
+import { Calendar } from "@/components/ui/calendar";
+import { Calendar as CalendarIcon } from "lucide-react";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { Label } from "../ui/label";
+import { createAppointment } from "@/app/(dashboard)/appointments/action";
+import toast from 'react-hot-toast';
+import { useAppointmentModal } from "@/hooks/useAppointmentModal";
+
+const CreateAppointment = () => {
+    const { isOpen, onClose, type } = useAppointmentModal();
+    const isModalOpen = isOpen && type === "createAppointment";
+
+    const [date, setDate] = useState<Date | undefined>(new Date());
+
+    const handleDialogChange = () => {
+        onClose();
+    };
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const formData = new FormData();
+            if (date) formData.append("date", date.toISOString());
+
+            const response = await createAppointment(formData);
+            if (response.success) {
+                toast.success("Create appointment successfully!");
+                onClose();
+            }
+        } catch (error) {
+            console.log("Error creating appointment: ", error);
+        }
+    };
+
+    return (
+        <Dialog open={isModalOpen} onOpenChange={handleDialogChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create Appointment</DialogTitle>
+                </DialogHeader>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+                    <Label htmlFor="date">Date</Label>
+                    <Popover>
+                        <PopoverTrigger id="date" asChild>
+                            <Button
+                                variant={"outline"}
+                                className={cn(
+                                    "w-[280px] justify-start text-left font-normal",
+                                    !date && "text-muted-foreground"
+                                )}
+                            >
+                                <CalendarIcon className="mr-2 h-4 w-4" />
+                                {date ? (
+                                    format(date, "PPP")
+                                ) : (
+                                    <span>Pick a date</span>
+                                )}
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0">
+                            <Calendar
+                                mode="single"
+                                selected={date}
+                                onSelect={setDate}
+                                initialFocus
+                            />
+                        </PopoverContent>
+                    </Popover>
+                    <DialogFooter className="mt-6">
+                        <Button
+                            type="button"
+                            variant="ghost"
+                            onClick={handleDialogChange}
+                        >
+                            Cancel
+                        </Button>
+                        <Button type="submit" variant="secondary">
+                            Create
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default CreateAppointment;
