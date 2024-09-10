fa4ad327c566f10a569e763014579f691290ec83
nerwinalamas <nerwinalamas@gmail.com>
2024-09-10 10:11:18 +0800

    styles: adjust font weight

--- app/appointment/_components/appointment-card.tsx
+++ app/appointment/_components/appointment-card.tsx
@@ -11,8 +11,8 @@ const AppointmentCard = ({ id, date, available_slots }: AppointmentProps) => {
             className="flex flex-col justify-between h-40 p-4 rounded-lg bg-slate-100 text-slate-800"
         >
             <div className="flex flex-col gap-1">
-                <h1 className="text-xl font-semibold uppercase">{dayName}</h1>
-                <h2 className="text-sm font-bold">{formattedDate}</h2>
+                <h1 className="text-xl font-medium uppercase">{dayName}</h1>
+                <h2 className="text-sm font-semibold">{formattedDate}</h2>
             </div>
             {available_slots === 0 ? (
                 <p className="text-sm uppercase">Fully Booked</p>
