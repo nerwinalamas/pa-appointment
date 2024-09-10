502df658412ca2f05b4d7875b679ec13a9afeede
nerwinalamas <nerwinalamas@gmail.com>
2024-09-10 10:08:54 +0800

    feat: hook for appointment modal

---
+++ hooks/useAppointmentModal.tsx
@@ -0,0 +1,23 @@
+import { AppointmentProps } from "@/app/appointment/_types";
+import { create } from "zustand";
+
+export type ModalType =
+    | "createAppointment"
+    | "updateAppointment";
+
+type ModalStore = {
+    type: ModalType | null;
+    data?: AppointmentProps;
+    isOpen: boolean;
+    onOpen: (type: ModalType, data?: AppointmentProps) => void;
+    onClose: () => void;
+};
+
+export const useAppointmentModal = create<ModalStore>((set) => ({
+    type: null,
+    dataObject: {},
+    isOpen: false,
+    onOpen: (type, data?: AppointmentProps) =>
+        set({ isOpen: true, type, data }),
+    onClose: () => set({ type: null, isOpen: false }),
+}));
