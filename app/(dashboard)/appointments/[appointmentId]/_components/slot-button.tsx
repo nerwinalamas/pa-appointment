fa1c6331191e3747b33e47e490790496f441b900
nerwinalamas <nerwinalamas@gmail.com>
2024-09-10 10:12:34 +0800

    feat: create button for slot

---
+++ app/(dashboard)/appointments/[appointmentId]/_components/slot-button.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useSlotModal } from "@/hooks/useSlotModal";
+
+const SlotButton = ({ appointmentId }: { appointmentId: string }) => {
+    const { onOpen } = useSlotModal();
+
+    return (
+        <Button onClick={() => onOpen("createSlot", appointmentId as string)}>
+            Create Slot
+        </Button>
+    );
+};
+
+export default SlotButton;
