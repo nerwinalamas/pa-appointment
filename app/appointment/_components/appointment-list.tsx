1c5c6b67754b0b9439e4c996d0a15a096652d4c9
nerwinalamas <nerwinalamas@gmail.com>
2024-09-12 06:41:16 +0800

    refactor: add default message and remove filter

--- app/appointment/_components/appointment-list.tsx
+++ app/appointment/_components/appointment-list.tsx
@@ -2,32 +2,22 @@ import AppointmentCard from "./appointment-card";
 import { AppointmentProps } from "../_types";
 
 const AppointmentList = ({ data }: { data: AppointmentProps[] }) => {
-    const currentDate = new Date();
-    currentDate.setHours(0, 0, 0, 0);
-    const currentMonth = currentDate.getMonth();
-    const currentYear = currentDate.getFullYear();
-
-    const filteredData = data
-        .filter((item) => {
-            const itemDate = new Date(item.date);
-            itemDate.setHours(0, 0, 0, 0);
-            return (
-                itemDate.getMonth() === currentMonth &&
-                itemDate.getFullYear() === currentYear &&
-                itemDate >= currentDate
-            );
-        })
-        .sort(
-            (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
-        );
-
     return (
         <div className="max-w-screen-xl mx-auto p-4 grid grid-cols-2 gap-4 md:grid-cols-3 lg:w-[80%] xl:grid-cols-6">
-            {filteredData.map((appointment) => {
-                return (
-                    <AppointmentCard key={appointment.id} {...appointment} />
-                );
-            })}
+            {data.length === 0 ? (
+                <h1 className="col-span-full text-center text-gray-500">
+                    No Appointment Dates Available
+                </h1>
+            ) : (
+                data.map((appointment) => {
+                    return (
+                        <AppointmentCard
+                            key={appointment.id}
+                            {...appointment}
+                        />
+                    );
+                })
+            )}
         </div>
     );
 };
