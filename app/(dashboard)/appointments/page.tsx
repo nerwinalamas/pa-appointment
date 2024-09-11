563412f3443b77193f5919b2962cdb9d11ae2670
nerwinalamas <nerwinalamas@gmail.com>
2024-09-12 06:38:32 +0800

    refactor: add pagination and default message

--- app/(dashboard)/appointments/page.tsx
+++ app/(dashboard)/appointments/page.tsx
@@ -2,25 +2,36 @@ import { formatDate } from "@/app/appointment/_lib/utils";
 import {
     Table,
     TableBody,
+    TableCell,
+    TableFooter,
     TableHead,
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
-import { createClient } from "@/utils/supabase/server";
 import AppointmentButton from "./_components/appointment-button";
 import AppointmentTableBody from "./_components/appointment-table-body";
+import AppointmentPagination from "./_components/appointment-pagination";
+import { getAllAppointmentsDashboard } from "./service";
+import { Suspense } from "react";
 
-const Appointments = async () => {
-    const supabase = await createClient();
-    const { data, error } = await supabase.from("appointments").select("*");
+const Appointments = async ({
+    searchParams,
+}: {
+    searchParams: { page: string };
+}) => {
+    const page = parseInt(searchParams.page) || 1;
+    const pageSize = 10;
+
+    const { data, error, count } = await getAllAppointmentsDashboard(
+        page,
+        pageSize
+    );
 
     if (error) {
         return <h1>Error appointment dashboard</h1>;
     }
 
-    const filteredData = data.sort(
-        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
-    );
+    const totalPages = Math.ceil(count! / pageSize);
 
     return (
         <div className="w-[60%] flex flex-col items-end gap-2">
@@ -42,21 +53,50 @@ const Appointments = async () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {filteredData?.map((appointment) => {
-                        const { dayName, formattedDate } = formatDate(
-                            appointment.date
-                        );
-                        return (
-                            <AppointmentTableBody
-                                key={appointment.id}
-                                appointment={appointment}
-                                formattedDate={formattedDate}
-                                dayName={dayName}
-                            />
-                        );
-                    })}
+                    {data?.length === 0 ? (
+                        <TableRow>
+                            <TableCell
+                                colSpan={5}
+                                className="text-center text-gray-500"
+                            >
+                                No Appointment Dates Available
+                            </TableCell>
+                        </TableRow>
+                    ) : (
+                        data?.map((appointment) => {
+                            const { dayName, formattedDate } = formatDate(
+                                appointment.date
+                            );
+                            return (
+                                <AppointmentTableBody
+                                    key={appointment.id}
+                                    appointment={appointment}
+                                    formattedDate={formattedDate}
+                                    dayName={dayName}
+                                />
+                            );
+                        })
+                    )}
                 </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell colSpan={4}>
+                            Page {page} of {totalPages}
+                        </TableCell>
+                        <TableCell className="text-right">
+                            {count} rows
+                        </TableCell>
+                    </TableRow>
+                </TableFooter>
             </Table>
+            {count! > pageSize && (
+                <Suspense fallback={<div>Loading pagination...</div>}>
+                    <AppointmentPagination
+                        currentPage={page}
+                        totalPages={totalPages}
+                    />
+                </Suspense>
+            )}
         </div>
     );
 };
