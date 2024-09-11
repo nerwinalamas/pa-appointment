8fefcfc112a6c920b58a6f3b337f4060ec580b41
nerwinalamas <nerwinalamas@gmail.com>
2024-09-12 06:45:30 +0800

    refactor: card

--- app/appointment/_components/appointment-card.tsx
+++ app/appointment/_components/appointment-card.tsx
@@ -1,21 +1,16 @@
 import Link from "next/link";
 import { AppointmentProps } from "../_types";
 import { formatDate } from "../_lib/utils";
-import { getSlotCount } from "../service";
 
 const AppointmentCard = async ({
     id,
     date,
     available_slots,
+    slotCount
 }: AppointmentProps) => {
     const { dayName, formattedDate } = formatDate(date);
-    const { count, error } = await getSlotCount(id);
 
-    if (error) {
-        return <h1 className="text-slate-200">Error: cannot get count</h1>;
-    }
-
-    if (count === 0) {
+    if (slotCount === 0 && available_slots === 0) {
         return (
             <div className="flex flex-col justify-between h-40 p-4 rounded-lg cursor-not-allowed bg-gray-400 text-slate-800">
                 <div className="flex flex-col gap-1">
@@ -27,7 +22,7 @@ const AppointmentCard = async ({
         );
     }
 
-    if (available_slots === 0) {
+    if (slotCount !== 0 && available_slots === 0) {
         return (
             <div className="flex flex-col justify-between h-40 p-4 rounded-lg cursor-not-allowed bg-red-400 text-slate-800">
                 <div className="flex flex-col gap-1">
@@ -43,7 +38,7 @@ const AppointmentCard = async ({
         <Link
             href={`/appointment/${id}`}
             className={`flex flex-col justify-between h-40 p-4 rounded-lg ${
-                count === available_slots && count !== 0
+                slotCount === available_slots && slotCount !== 0
                     ? "bg-green-400"
                     : "bg-yellow-400"
             } text-slate-800`}
@@ -52,7 +47,7 @@ const AppointmentCard = async ({
                 <h1 className="text-xl font-medium uppercase">{dayName}</h1>
                 <h2 className="text-sm font-semibold">{formattedDate}</h2>
             </div>
-            {count === available_slots && count !== 0 ? (
+            {slotCount === available_slots && slotCount !== 0 ? (
                 <p className="text-sm uppercase">
                     {available_slots} Slots Available
                 </p>
