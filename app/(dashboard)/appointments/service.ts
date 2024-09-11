4f53decb0cddc5e8a6f11f237d30f98ab5f2746e
nerwinalamas <nerwinalamas@gmail.com>
2024-09-12 06:39:06 +0800

    feat: get all appointments in dashboard

---
+++ app/(dashboard)/appointments/service.ts
@@ -0,0 +1,20 @@
+import { createClient } from "@/utils/supabase/server";
+
+export const getAllAppointmentsDashboard = async (page = 1, pageSize = 10) => {
+    const supabase = await createClient();
+
+    const start = (page - 1) * pageSize;
+    const end = start + pageSize - 1;
+
+    const { data, error, count } = await supabase
+        .from("appointments")
+        .select("*", { count: "exact" })
+        .range(start, end)
+        .order("date", { ascending: true });
+
+    if (error) {
+        return { success: false, error: error.message };
+    }
+
+    return { success: true, data, count, page, pageSize };
+};
