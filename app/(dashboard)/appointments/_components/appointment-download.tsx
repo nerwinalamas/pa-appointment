0ac53d09edf66ffe7b4b6cae395ee29b435a0ba8
nerwinalamas <nerwinalamas@gmail.com>
2024-09-13 09:44:38 +0800

    feat: download button

---
+++ app/(dashboard)/appointments/_components/appointment-download.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { downloadAppointments } from "../download-action";
+
+// TODO: NOT FINAL
+const AppointmentDownload = () => {
+    const handleDownload = async () => {
+        const response = await downloadAppointments();
+        if (response.success) {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement("a");
+            link.href = url;
+            link.setAttribute("download", "appointments.xlsx");
+            document.body.appendChild(link);
+            link.click();
+            link.parentNode?.removeChild(link);
+        } else {
+            console.error(response.error);
+        }
+    };
+
+    return <Button onClick={handleDownload}>Download</Button>;
+};
+
+export default AppointmentDownload;
