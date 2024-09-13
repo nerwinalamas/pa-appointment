a43b547b56f33fad7a7e941658504d45c848d40e
nerwinalamas <nerwinalamas@gmail.com>
2024-09-13 09:41:58 +0800

    feat: add validation using zod

--- app/appointment/[appointmentDateId]/[appointmentTimeId]/_components/payment-form.tsx
+++ app/appointment/[appointmentDateId]/[appointmentTimeId]/_components/payment-form.tsx
@@ -3,6 +3,8 @@
 import { useRouter } from "next/navigation";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { reserveSlot } from "../action";
+import { userSchema } from "../_lib/schema";
+import { ErrorMessage } from "../_lib/type";
 
 const PaymentForm = ({
     appointmentDateId,
@@ -16,6 +18,7 @@ const PaymentForm = ({
     const [depositScreenshot, setDepositScreenshot] = useState<File | null>(
         null
     );
+    const [error, setError] = useState<ErrorMessage>({});
 
     const router = useRouter();
 
@@ -23,21 +26,41 @@ const PaymentForm = ({
         if (e.target.files && e.target.files[0]) {
             setDepositScreenshot(e.target.files[0]);
         }
+        setError((prev) => ({ ...prev, depositScreenshot: "" }));
     };
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
 
-        if (!depositScreenshot) {
-            console.log("Error: No deposit screenshot uploaded");
+        const formData = {
+            name,
+            contactNumber,
+            depositScreenshot,
+        };
+
+        const result = userSchema.safeParse(formData);
+
+        if (!result.success) {
+            const formattedErrors = result.error.flatten().fieldErrors;
+            type ErrorKeys = keyof typeof formattedErrors;
+            setError(
+                Object.keys(formattedErrors).reduce((acc, key) => {
+                    const typedKey = key as ErrorKeys;
+                    acc[typedKey] = formattedErrors[typedKey]?.[0] || "";
+                    return acc;
+                }, {} as ErrorMessage)
+            );
             return;
         }
+
+        setError({});
 
         try {
             const formData = new FormData();
             formData.append("name", name);
             formData.append("contactNumber", contactNumber);
-            formData.append("depositScreenshot", depositScreenshot);
+            if (depositScreenshot)
+                formData.append("depositScreenshot", depositScreenshot);
 
             const reponse = await reserveSlot(
                 appointmentDateId as string,
@@ -74,10 +97,16 @@ const PaymentForm = ({
                         name="name"
                         placeholder="Name"
                         value={name}
-                        onChange={(e) => setName(e.target.value)}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                            setError((prev) => ({ ...prev, name: "" }));
+                        }}
                         className="p-3 rounded-md text-slate-800 outline-none"
                     />
                 </div>
+                {error.name && (
+                    <p className="text-sm text-red-500">{error.name}</p>
+                )}
                 <div className="flex flex-col gap-2">
                     <label htmlFor="contactNumber">Contact Number</label>
                     <input
@@ -86,10 +115,21 @@ const PaymentForm = ({
                         name="contactNumber"
                         placeholder="Contact Number"
                         value={contactNumber}
-                        onChange={(e) => setContactNumber(e.target.value)}
+                        onChange={(e) => {
+                            setContactNumber(e.target.value);
+                            setError((prev) => ({
+                                ...prev,
+                                contactNumber: "",
+                            }));
+                        }}
                         className="p-3 rounded-md text-slate-800 outline-none"
                     />
                 </div>
+                {error.contactNumber && (
+                    <p className="text-sm text-red-500">
+                        {error.contactNumber}
+                    </p>
+                )}
                 <div className="flex flex-col gap-2">
                     <label htmlFor="depositScreenshot">
                         Deposit Screenshots
@@ -102,6 +142,11 @@ const PaymentForm = ({
                         onChange={handleFileChange}
                     />
                 </div>
+                {error.depositScreenshot && (
+                    <p className="text-sm text-red-500">
+                        {error.depositScreenshot}
+                    </p>
+                )}
             </div>
             <button
                 type="submit"
