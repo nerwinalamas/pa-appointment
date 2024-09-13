821942ec965a5b9cc929dd8c55e52c539004b928
nerwinalamas <nerwinalamas@gmail.com>
2024-09-13 09:38:15 +0800

    styles: make it responsive to different screen sizes

--- app/(dashboard)/appointments/[appointmentId]/page.tsx
+++ app/(dashboard)/appointments/[appointmentId]/page.tsx
@@ -3,6 +3,7 @@ import {
     Table,
     TableBody,
     TableCell,
+    TableFooter,
     TableHead,
     TableHeader,
     TableRow,
@@ -17,33 +18,37 @@ const AppointmentId = async ({
 }: {
     params: { appointmentId: string };
 }) => {
-    const { data, error } = await getAllSlotsDashboard(params.appointmentId);
+    const { data, error, count } = await getAllSlotsDashboard(params.appointmentId);
 
     if (error) {
         return <h1>Error slots dashboard</h1>;
     }
 
     return (
-        <div className="w-[60%] flex flex-col items-end gap-2">
-            <BackButton />
-            <SlotButton appointmentId={params.appointmentId} />
-            <Table>
-                <TableHeader>
-                    <TableRow>
+        <div className="flex flex-col items-end gap-2 mt-5 mb-20 lg:gap-0 lg:w-[90%] lg:mx-auto xl:w-[65%]">
+            <div className="w-full p-4 flex justify-between items-center gap-2">
+                <BackButton />
+                <SlotButton appointmentId={params.appointmentId} />
+            </div>
+            <Table className="grid gap-3 p-4 lg:gap-0">
+                <TableHeader className="hidden lg:grid">
+                    <TableRow className="lg:grid lg:grid-cols-10">
                         <TableHead className="text-center">
                             Start time
                         </TableHead>
                         <TableHead className="text-center">End time</TableHead>
                         <TableHead className="text-center">Status</TableHead>
-                        <TableHead className="text-center">Name</TableHead>
-                        <TableHead className="text-center">
+                        <TableHead className="text-center lg:col-span-2">Name</TableHead>
+                        <TableHead className="text-center lg:col-span-2">
                             Contact Number
                         </TableHead>
                         <TableHead className="text-center">Deposit</TableHead>
-                        <TableHead className="text-center">Actions</TableHead>
+                        <TableHead className="text-center lg:col-span-2">
+                            Actions
+                        </TableHead>
                     </TableRow>
                 </TableHeader>
-                <TableBody>
+                <TableBody className="grid gap-3 md:grid-cols-2 lg:grid-cols-1 lg:gap-0">
                     {data?.length === 0 ? (
                         <TableRow>
                             <TableCell
@@ -71,6 +76,13 @@ const AppointmentId = async ({
                         })
                     )}
                 </TableBody>
+                <TableFooter>
+                    <TableRow className="grid">
+                        <TableCell className="text-right">
+                            {count} rows
+                        </TableCell>
+                    </TableRow>
+                </TableFooter>
             </Table>
         </div>
     );
