550df3be5b40b7ee105f893f91fa171aaf203124
nerwinalamas <nerwinalamas@gmail.com>
2024-09-13 09:41:36 +0800

    styles: update width

--- app/appointment/_components/appointment-list.tsx
+++ app/appointment/_components/appointment-list.tsx
@@ -3,7 +3,7 @@ import { AppointmentProps } from "../_types";
 
 const AppointmentList = ({ data }: { data: AppointmentProps[] }) => {
     return (
-        <div className="max-w-screen-xl mx-auto p-4 grid grid-cols-2 gap-4 md:grid-cols-3 lg:w-[80%] xl:grid-cols-6">
+        <div className="max-w-screen-xl mx-auto p-4 grid grid-cols-2 gap-4 md:w-[80%] md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
             {data.length === 0 ? (
                 <h1 className="col-span-full text-center text-gray-500">
                     No Appointment Dates Available
