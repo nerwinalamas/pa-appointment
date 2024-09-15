ff8e834c8977bdba00e3bc0b163c359b494a12bb
nerwinalamas <nerwinalamas@gmail.com>
2024-09-15 08:47:09 +0800

    styles: add hover and focus

--- app/(dashboard)/appointments/_components/appointment-button.tsx
+++ app/(dashboard)/appointments/_components/appointment-button.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useAppointmentModal } from "@/hooks/useAppointmentModal";
+import { Button } from "@/components/ui/button";
 
 const AppointmentButton = () => {
     const { onOpen } = useAppointmentModal();
-    
-    return <Button variant="secondary" onClick={() => onOpen("createAppointment")}>Create</Button>;
+
+    return (
+        <Button
+            variant="secondary"
+            onClick={() => onOpen("createAppointment")}
+            className="hover:bg-slate-200 focus:bg-slate-200 dark:hover:bg-slate-700 dark:focus:bg-slate-700"
+        >
+            Create
+        </Button>
+    );
 };
 
 export default AppointmentButton;
