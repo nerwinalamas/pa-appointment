ace86a436ca989d8f450d61d4d4a48ad80de808c
nerwinalamas <nerwinalamas@gmail.com>
2024-09-15 08:43:34 +0800

    refactor: login action

--- app/(auth)/login/actions.ts
+++ app/(auth)/login/actions.ts
@@ -1,28 +1,23 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-
 import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
 
 export async function login(formData: FormData) {
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
     const supabase = createClient();
-
-    // type-casting here for convenience
-    // in practice, you should validate your inputs
-    const data = {
-        email: formData.get("email") as string,
-        password: formData.get("password") as string,
-    };
-
-    const { error } = await supabase.auth.signInWithPassword(data);
+    const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
 
     if (error) {
         console.log("error message: ", error.message);
-        console.log("error status: ", error.status);
-        // redirect('/error')
+        return { succes: false, error: error.message };
     }
 
-    revalidatePath("/", "layout");
-    redirect("/appointments");
+    revalidatePath("/login");
+    return { success: true };
 }
