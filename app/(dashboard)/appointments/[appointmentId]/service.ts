b12122574d3ad0d95da55ad1c666b2e1a184e816
nerwinalamas <nerwinalamas@gmail.com>
2024-09-15 08:46:22 +0800

    feat: get appointment date by id action

--- app/(dashboard)/appointments/[appointmentId]/service.ts
+++ app/(dashboard)/appointments/[appointmentId]/service.ts
@@ -15,3 +15,19 @@ export const getAllSlotsDashboard = async (dateId: string) => {
 
     return { success: true, data, count };
 };
+
+export const getAppointmentDateById = async (dateId: string) => {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase
+        .from("appointments")
+        .select("*")
+        .eq("id", dateId)
+        .single();
+
+    if (error) {
+        return { success: false, error: error.message };
+    }
+
+    return { success: true, data };
+};
