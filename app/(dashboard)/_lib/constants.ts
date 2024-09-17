873a745a1a5ad648ca8480bf508a91b44d87710e
nerwinalamas <nerwinalamas@gmail.com>
2024-09-17 12:31:06 +0800

    feat: constants

---
+++ app/(dashboard)/_lib/constants.ts
@@ -0,0 +1,22 @@
+export const items = [
+    {
+        id: "Dashboard",
+        name: "Dashboard",
+        url: "/dashboard",
+    },
+    {
+        id: "Appointments",
+        name: "Appointments",
+        url: "/appointments",
+    },
+    {
+        id: "Staff",
+        name: "Staff",
+        url: "/staff",
+    },
+    {
+        id: "Account",
+        name: "Account",
+        url: "/account",
+    },
+];
