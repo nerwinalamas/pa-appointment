2e9d1ac7a0bcf73425d6fd05a2cb2324e1163154
nerwinalamas <nerwinalamas@gmail.com>
2024-09-18 16:10:34 +0800

    feat: dashboard bookings

---
+++ app/(dashboard)/dashboard/_components/dashboard-bookings.tsx
@@ -0,0 +1,17 @@
+import { ArrowUp } from "lucide-react";
+
+const DashboardBookings = () => {
+    return (
+        <div className="flex flex-col gap-1 p-4 rounded-md bg-slate-100 dark:bg-slate-950">
+            <h2 className="text-base font-light">Bookings</h2>
+            <p className="text-xl font-bold">50</p>
+            <div className="flex items-center gap-1 text-green-500">
+                <ArrowUp size={18} strokeWidth={2} />
+                <p>10.4%</p>
+            </div>
+            <p className="text-xs text-slate-700">for this month</p>
+        </div>
+    );
+};
+
+export default DashboardBookings;
