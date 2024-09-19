87d071993590eda6c5923815233a80eb5cda9a46
nerwinalamas <nerwinalamas@gmail.com>
2024-09-19 09:19:35 +0800

    refactor: get all total bookings

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -5,8 +5,15 @@ import DashboardTotalSales from "./_components/dashboard-total-sales";
 import DashboardSlotAreaChart from "./_components/dashboard-slot-area-chart";
 import DashboardTable from "./_components/dashboard-table";
 import DashboardTotalBookingsChart from "./_components/dashboard-total-bookings-chart";
+import { getAllTotalBookings } from "./service";
 
-const Dashboard = () => {
+const Dashboard = async () => {
+    const { count, error } = await getAllTotalBookings();
+    
+    if (error) {
+        return <h2>Error par</h2>;
+    }
+
     return (
         <div className="p-4 grid gap-4 grid-rows-10 md:grid-rows-8 md:grid-cols-2 xl:grid-rows-4 xl:grid-cols-4">
             <DashboardBookings />
@@ -15,7 +22,7 @@ const Dashboard = () => {
             <DashboardTotalSales />
             <DashboardSlotAreaChart />
             <DashboardTable />
-            <DashboardTotalBookingsChart />
+            <DashboardTotalBookingsChart count={count as number} />
         </div>
     );
 };
