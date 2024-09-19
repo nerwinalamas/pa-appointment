36d3e162884e274f2477e3e12352abbccdb10425
nerwinalamas <nerwinalamas@gmail.com>
2024-09-19 09:16:55 +0800

    feat: staff type

---
+++ app/(dashboard)/staff/_types/index.ts
@@ -0,0 +1,15 @@
+export type Staff = {
+    name: string;
+    contact_number: string;
+    email: string;
+    role: string;
+    status: string;
+};
+
+export type ErrorState = {
+    name?: string;
+    contact_number?: string;
+    email?: string;
+    role?: string;
+    status?: string;
+};
