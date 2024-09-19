ec2e32e582d275b588edb7bb8c1b8eceb154f769
nerwinalamas <nerwinalamas@gmail.com>
2024-09-19 09:17:35 +0800

    refactor: staff page

--- app/(dashboard)/staff/page.tsx
+++ app/(dashboard)/staff/page.tsx
@@ -6,83 +6,72 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
+import StaffCreateButton from "./_components/staff-create-button";
+import StaffTableRow from "./_components/staff-table-row";
+
+const data = [
+    {
+        id: "1",
+        name: "Mary Doe",
+        contact_number: "09123456798",
+        email: "md@gmail.com",
+        role: "Admin",
+        status: "Inactive",
+    },
+    {
+        id: "2",
+        name: "John Doe",
+        contact_number: "09123456798",
+        email: "jd@gmail.com",
+        role: "Super Admin",
+        status: "Active",
+    },
+    {
+        id: "3",
+        name: "Julia Doe",
+        contact_number: "09123456798",
+        email: "juliadoe@gmail.com",
+        role: "Admin",
+        status: "Active",
+    },
+];
 
 const Staff = () => {
     return (
-        <Table className="p-4">
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Contact Number</TableHead>
-                    <TableHead>Email</TableHead>
-                    <TableHead>Role</TableHead>
-                    <TableHead className="text-center">Action</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                <TableRow>
-                    <TableCell>Mary Doe</TableCell>
-                    <TableCell>09123456798</TableCell>
-                    <TableCell>md@gmail.com</TableCell>
-                    <TableCell>Admin</TableCell>
-                    <TableCell className="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-center lg:col-span-2">
-                        <Button
-                            variant="secondary"
-                            className="hover:bg-slate-200 focus:bg-slate-200 dark:hover:bg-slate-700 dark:focus:bg-slate-700"
-                        >
-                            Update
-                        </Button>
-                        <Button
-                            variant="secondary"
-                            className="hover:bg-slate-200 focus:bg-slate-200 dark:hover:bg-slate-700 dark:focus:bg-slate-700"
-                        >
-                            Delete
-                        </Button>
-                    </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell>John Doe</TableCell>
-                    <TableCell>09123456798</TableCell>
-                    <TableCell>jd@gmail.com</TableCell>
-                    <TableCell>Super Admin</TableCell>
-                    <TableCell className="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-center lg:col-span-2">
-                        <Button
-                            variant="secondary"
-                            className="hover:bg-slate-200 focus:bg-slate-200 dark:hover:bg-slate-700 dark:focus:bg-slate-700"
-                        >
-                            Update
-                        </Button>
-                        <Button
-                            variant="secondary"
-                            className="hover:bg-slate-200 focus:bg-slate-200 dark:hover:bg-slate-700 dark:focus:bg-slate-700"
-                        >
-                            Delete
-                        </Button>
-                    </TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell>Julia Doe</TableCell>
-                    <TableCell>09123456798</TableCell>
-                    <TableCell>juliadoe@gmail.com</TableCell>
-                    <TableCell>Admin</TableCell>
-                    <TableCell className="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-center lg:col-span-2">
-                        <Button
-                            variant="secondary"
-                            className="hover:bg-slate-200 focus:bg-slate-200 dark:hover:bg-slate-700 dark:focus:bg-slate-700"
-                        >
-                            Update
-                        </Button>
-                        <Button
-                            variant="secondary"
-                            className="hover:bg-slate-200 focus:bg-slate-200 dark:hover:bg-slate-700 dark:focus:bg-slate-700"
-                        >
-                            Delete
-                        </Button>
-                    </TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
+        <div className="flex flex-col items-end gap-2 pt-5 pb-20 lg:pb-12 lg:gap-0 lg:mx-auto xl:m-4 xl:p-4 bg-slate-950">
+            <div className="w-full p-4 flex justify-end items-center gap-2">
+                <StaffCreateButton />
+            </div>
+            <Table className="p-4">
+                <TableHeader className="hidden xl:grid">
+                    <TableRow className="grid xl:grid-cols-7">
+                        <TableHead>Name</TableHead>
+                        <TableHead className="text-center">
+                            Contact Number
+                        </TableHead>
+                        <TableHead className="text-center">Email</TableHead>
+                        <TableHead className="text-center">Role</TableHead>
+                        <TableHead className="text-center">Status</TableHead>
+                        <TableHead className="text-center xl:col-span-2">
+                            Action
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody className="grid gap-3 md:grid-cols-2 xl:grid-cols-1 xl:gap-0">
+                    {data?.length === 0 ? (
+                        <TableRow>
+                            <TableCell colSpan={5} className="text-center">
+                                No Staff
+                            </TableCell>
+                        </TableRow>
+                    ) : (
+                        data?.map((staff) => {
+                            return <StaffTableRow key={staff.id} {...staff} />;
+                        })
+                    )}
+                </TableBody>
+            </Table>
+        </div>
     );
 };
 
