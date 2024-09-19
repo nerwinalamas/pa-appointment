946deaba37a0c4d3a0be259b4b6624138e63e54f
nerwinalamas <nerwinalamas@gmail.com>
2024-09-19 09:15:50 +0800

    feat: staff modal

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import CreateAppointment from "@/components/modals/CreateAppointment";
-import DeleteAppointment from "@/components/modals/DeleteAppointment";
 import UpdateAppointment from "@/components/modals/UpdateAppointment";
+import DeleteAppointment from "@/components/modals/DeleteAppointment";
 import CreateSlot from "@/components/modals/CreateSlot";
-import DeleteSlot from "@/components/modals/DeleteSlot";
 import UpdateSlot from "@/components/modals/UpdateSlot";
+import DeleteSlot from "@/components/modals/DeleteSlot";
+import CreateStaff from "@/components/modals/CreateStaff";
+import UpdateStaff from "@/components/modals/UpdateStaff";
+import DeleteStaff from "@/components/modals/DeleteStaff";
 
 const ModalProvider = () => {
     return (
@@ -13,9 +16,14 @@ const ModalProvider = () => {
             <CreateAppointment />
             <UpdateAppointment />
             <DeleteAppointment />
+
             <CreateSlot />
             <UpdateSlot />
             <DeleteSlot />
+
+            <CreateStaff />
+            <UpdateStaff />
+            <DeleteStaff />
         </>
     );
 };
