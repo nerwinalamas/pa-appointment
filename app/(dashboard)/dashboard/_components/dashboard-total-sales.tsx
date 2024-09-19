bfaa0b30fde45c5e94df33c97281871113012131
nerwinalamas <nerwinalamas@gmail.com>
2024-09-19 09:22:48 +0800

    refactor: get all total slots times deposit amount

--- app/(dashboard)/dashboard/_components/dashboard-total-sales.tsx
+++ app/(dashboard)/dashboard/_components/dashboard-total-sales.tsx
@@ -1,10 +1,18 @@
+import { DEPOSIT_AMOUNT } from "../_lib/constant";
+import { getAllTotalSlots } from "../service";
 import { ArrowUp } from "lucide-react";
 
-const DashboardTotalSales = () => {
+const DashboardTotalSales = async () => {
+    const { count, error } = await getAllTotalSlots();
+
+    if (error) {
+        return <h2>Error par</h2>;
+    }
+
     return (
         <div className="flex flex-col gap-1 p-4 rounded-md bg-slate-100 dark:bg-slate-950 xl:row-start-2">
             <h2 className="text-base font-light">Total Sales</h2>
-            <p className="text-xl font-bold">50</p>
+            <p className="text-xl font-bold">{count as number * DEPOSIT_AMOUNT}</p>
             <div className="flex items-center gap-1 text-green-500">
                 <ArrowUp size={18} strokeWidth={2} />
                 <p>10.4%</p>
