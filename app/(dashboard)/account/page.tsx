14e18bc49911d64e595dc43977276205de69b109
nerwinalamas <nerwinalamas@gmail.com>
2024-09-20 17:01:54 +0800

    refactor: account page

--- app/(dashboard)/account/page.tsx
+++ app/(dashboard)/account/page.tsx
@@ -1,5 +1,9 @@
 const Account = () => {
-    return <div>Account</div>;
+    return (
+        <div className="flex flex-col items-end gap-2 pt-5 pb-20 lg:pb-12 lg:gap-0 lg:mx-auto xl:m-4 xl:p-4 bg-slate-100 dark:bg-slate-950">
+            Account
+        </div>
+    );
 };
 
 export default Account;
