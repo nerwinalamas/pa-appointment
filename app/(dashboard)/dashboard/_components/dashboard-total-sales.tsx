e026830655ca8ed38d0b35c291cf56ab148d18e1
nerwinalamas <nerwinalamas@gmail.com>
2024-09-23 08:30:55 +0800

    refactor: increase font size

--- app/(dashboard)/dashboard/_components/dashboard-total-sales.tsx
+++ app/(dashboard)/dashboard/_components/dashboard-total-sales.tsx
@@ -12,7 +12,7 @@ const DashboardTotalSales = async () => {
     return (
         <div className="flex flex-col gap-1 p-4 rounded-md bg-slate-100 dark:bg-slate-950 xl:row-start-2">
             <h2 className="text-base font-light">Total Sales</h2>
-            <p className="text-xl font-bold">{count as number * DEPOSIT_AMOUNT}</p>
+            <p className="text-2xl font-bold">{count as number * DEPOSIT_AMOUNT}</p>
             <div className="flex items-center gap-1 text-green-500">
                 <ArrowUp size={18} strokeWidth={2} />
                 <p>10.4%</p>
