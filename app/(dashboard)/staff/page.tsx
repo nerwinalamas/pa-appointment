e8c90b9c8ef3f96bb4b947ffabb96b82629a7c79
nerwinalamas <nerwinalamas@gmail.com>
2024-10-19 11:07:55 +0800

    refactor: staff page

--- app/(dashboard)/staff/page.tsx
+++ app/(dashboard)/staff/page.tsx
@@ -1,5 +1,6 @@
 // import { Suspense } from "react";
 // import PageHandler from "@/components/shared/page-handler";
+import { createClient } from "@/utils/supabase/server";
 import StaffCreateButton from "./_components/staff-create-button";
 import StaffTableRow from "./_components/staff-table-row";
 
@@ -12,34 +13,15 @@ import {
     TableRow,
 } from "@/components/ui/table";
 
-const data = [
-    {
-        id: "1",
-        name: "Mary Doe",
-        contact_number: "09123456798",
-        email: "md@gmail.com",
-        role: "Admin",
-        status: "Inactive",
-    },
-    {
-        id: "2",
-        name: "John Doe",
-        contact_number: "09123456798",
-        email: "jd@gmail.com",
-        role: "Super Admin",
-        status: "Active",
-    },
-    {
-        id: "3",
-        name: "Julia Doe",
-        contact_number: "09123456798",
-        email: "juliadoe@gmail.com",
-        role: "Admin",
-        status: "Active",
-    },
-];
+const Staff = async () => {
+    const supabase = createClient();
+    const { data, error } = await supabase.from("staff").select("*");
 
-const Staff = () => {
+    if (error) {
+        console.log("Error fetching staff: ", error.message);
+        return <h1>Error: {error.message}</h1>;
+    }
+
     return (
         <div className="flex flex-col items-end gap-2 pt-5 pb-20 lg:pb-12 lg:gap-0 lg:mx-auto xl:m-4 xl:p-4 bg-slate-100 dark:bg-slate-950">
             <div className="w-full p-4 flex justify-end items-center gap-2">
