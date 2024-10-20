cf0f0c7608a96b1fb9ba8acbaae2d00e738131c8
nerwinalamas <nerwinalamas@gmail.com>
2024-10-20 20:24:57 +0800

    refactor: delete appointment modal

--- components/modals/DeleteAppointment.tsx
+++ components/modals/DeleteAppointment.tsx
@@ -6,12 +6,15 @@ import { useAppointmentModal } from "@/hooks/useAppointmentModal";
 import {
     Dialog,
     DialogContent,
+    DialogDescription,
     DialogFooter,
     DialogHeader,
     DialogTitle,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import toast from "react-hot-toast";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 const DeleteAppointment = () => {
     const { isOpen, onClose, type, data } = useAppointmentModal();
@@ -19,6 +22,7 @@ const DeleteAppointment = () => {
     const { dayName, formattedDate } = data
         ? formatDate(data.date)
         : { dayName: "", formattedDate: "" };
+    const timeSlot = data ? JSON.parse(data?.time_slots as string) : "";
 
     const handleDialogChange = () => {
         onClose();
@@ -47,12 +51,48 @@ const DeleteAppointment = () => {
             <DialogContent aria-describedby={undefined}>
                 <DialogHeader>
                     <DialogTitle>Delete Appointment</DialogTitle>
+                    <DialogDescription>
+                        Are you sure you want to delete this appointment? This
+                        action cannot be undone.
+                    </DialogDescription>
                 </DialogHeader>
                 <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-                    <p>Are you sure you want to delete this appointment?</p>
-                    <p className="text-red-500">
-                        {formattedDate} - {dayName}
-                    </p>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="date">Date</Label>
+                        <Input
+                            id="date"
+                            value={`${formattedDate} - ${dayName}`}
+                            className="col-span-3"
+                            readOnly
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="timeSlot">Time Slot</Label>
+                        <Input
+                            id="timeSlot"
+                            value={`${timeSlot.start} to ${timeSlot.end}`}
+                            className="col-span-3"
+                            readOnly
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name">Name</Label>
+                        <Input
+                            id="name"
+                            value={data?.name}
+                            className="col-span-3 capitalize"
+                            readOnly
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="contactNumber">Contact Number</Label>
+                        <Input
+                            id="contactNumber"
+                            value={data?.contact_number}
+                            className="col-span-3"
+                            readOnly
+                        />
+                    </div>
                     <DialogFooter className="mt-6">
                         <Button
                             type="button"
