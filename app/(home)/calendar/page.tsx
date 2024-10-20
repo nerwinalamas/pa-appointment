7f15e658377eeba355fffc7cdb6b67176dc2deb4
nerwinalamas <nerwinalamas@gmail.com>
2024-10-20 10:08:01 +0800

    feat: calendar page

---
+++ app/(home)/calendar/page.tsx
@@ -0,0 +1,11 @@
+import CalendarDayPicker from "./_components/calendar-day-picker";
+
+const AppointmentCalendar = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <CalendarDayPicker />
+        </div>
+    );
+};
+
+export default AppointmentCalendar;
