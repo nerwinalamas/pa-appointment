c8c39455d286d423e5c689d7a3c6848edc36e08c
nerwinalamas <nerwinalamas@gmail.com>
2024-10-20 10:08:45 +0800

    feat: time slots page

---
+++ app/(home)/time-slots/page.tsx
@@ -0,0 +1,11 @@
+import TimeSlotsPicker from "./_components/time-slots-picker";
+
+const TimeSlots = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <TimeSlotsPicker />
+        </div>
+    );
+};
+
+export default TimeSlots;
