7685bbaf9bbe43cda16c87abe5a6d5211ef8546a
nerwinalamas <nerwinalamas@gmail.com>
2024-10-20 20:23:32 +0800

    refactor: schema

--- app/(dashboard)/appointments/_lib/schema.ts
+++ app/(dashboard)/appointments/_lib/schema.ts
@@ -1,5 +1,19 @@
 import { z } from "zod";
 
 export const appointmentSchema = z.object({
-    date: z.date().or(z.string().transform((dateStr) => new Date(dateStr))),
+    date: z
+        .date()
+        .or(z.string().transform((dateStr) => new Date(dateStr)))
+        .refine((date) => !isNaN(date.getTime()), {
+            message: "Required",
+        }),
+    timeSlots: z.object({
+        start: z.string().min(1, { message: "Start time is required" }),
+        end: z.string().min(1, { message: "End time is required" }),
+    }),
+    name: z.string().min(1, { message: "Required" }),
+    contactNumber: z.string().min(1, { message: "Required" }),
+    // depositScreenshot: z.instanceof(File, {
+    //     message: "Required",
+    // }),
 });
