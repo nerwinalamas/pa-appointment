fc3b35cb1e996bd9bdaeced029235f29ab175651
nerwinalamas <nerwinalamas@gmail.com>
2024-10-21 11:10:05 +0800

    refactor: dashboard available slots

--- app/(dashboard)/dashboard/_components/dashboard-available-slots.tsx
+++ app/(dashboard)/dashboard/_components/dashboard-available-slots.tsx
@@ -1,23 +1,17 @@
-import { getAppointmentByTodaysDate } from "../service";
-import { ArrowUp } from "lucide-react";
+import { SLOTS } from "../_lib/constant";
+import { getTodaysAppointment } from "../service";
 
 const DashboardAvailableSlots = async () => {
-    const { data, error } = await getAppointmentByTodaysDate();
+    const { count, error } = await getTodaysAppointment();
 
     if (error) {
         return <h2>Error par</h2>;
     }
 
-    const availableSlots = data && data.length > 0 ? data[0].available_slots : 0;
-
     return (
         <div className="flex flex-col gap-1 p-4 rounded-md bg-slate-100 dark:bg-slate-950">
             <h2 className="text-base font-light">Available Slots</h2>
-            <p className="text-2xl font-bold">{availableSlots}</p>
-            <div className="flex items-center gap-1 text-green-500">
-                <ArrowUp size={18} strokeWidth={2} />
-                <p>10.4%</p>
-            </div>
+            <p className="text-2xl font-bold">{SLOTS - (count as number)}</p>
             <p className="text-xs text-slate-700">for today</p>
         </div>
     );
