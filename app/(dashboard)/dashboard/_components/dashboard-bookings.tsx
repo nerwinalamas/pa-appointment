a29a30272e1e84b78eba963f5944662d60f238d7
nerwinalamas <nerwinalamas@gmail.com>
2024-10-21 14:10:00 +0800

    refactor: dashboard bookings

--- app/(dashboard)/dashboard/_components/dashboard-bookings.tsx
+++ app/(dashboard)/dashboard/_components/dashboard-bookings.tsx
@@ -1,4 +1,5 @@
 import { getTodaysAppointment } from "../service";
+import { CalendarDays } from "lucide-react";
 
 const DashboardBookings = async () => {
     const { count, error } = await getTodaysAppointment();
@@ -8,10 +9,15 @@ const DashboardBookings = async () => {
     }
 
     return (
-        <div className="flex flex-col gap-1 p-4 rounded-md bg-slate-100 dark:bg-slate-950">
-            <h2 className="text-base font-light">Bookings</h2>
-            <p className="text-2xl font-bold">{count}</p>
-            <p className="text-xs text-slate-700">for today</p>
+        <div className="flex flex-col justify-between gap-1 p-4 rounded-md bg-slate-100 dark:bg-slate-950">
+            <div className="flex justify-between items-center">
+                <h2 className="text-base font-light">Bookings</h2>
+                <CalendarDays className="h-4 w-4 text-muted-foreground" />
+            </div>
+            <div>
+                <p className="text-3xl font-bold">{count}</p>
+                <p className="text-xs text-muted-foreground">for today</p>
+            </div>
         </div>
     );
 };
