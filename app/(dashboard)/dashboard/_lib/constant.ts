4adc3e830d926fdd83d3e526922b7dd2830acc0a
nerwinalamas <nerwinalamas@gmail.com>
2024-10-21 14:12:28 +0800

    refactor: remove unused code

--- app/(dashboard)/dashboard/_lib/constant.ts
+++ app/(dashboard)/dashboard/_lib/constant.ts
@@ -1,13 +1,5 @@
 export const DEPOSIT_AMOUNT = 10;
 export const SLOTS = 8;
+export const TARGET_BOOKINGS = 100;
 
-export const chartData = [
-    { month: "January", booking: 186, availableSlots: 80 },
-    { month: "February", booking: 305, availableSlots: 200 },
-    { month: "March", booking: 237, availableSlots: 120 },
-    { month: "April", booking: 73, availableSlots: 190 },
-    { month: "May", booking: 209, availableSlots: 130 },
-    { month: "June", booking: 214, availableSlots: 140 },
-];
-
-export const chartData2 = [{ visitors: 200, fill: "var(--color-safari)" }];
+export const chartData2 = [{ visitors: TARGET_BOOKINGS, fill: "var(--color-safari)" }];
