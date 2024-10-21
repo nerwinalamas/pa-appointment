bd870558b8720c49e13e1a6775d26a3520e87e5f
nerwinalamas <nerwinalamas@gmail.com>
2024-10-21 14:12:48 +0800

    refactor: dashboard slot per month

--- app/(dashboard)/dashboard/_components/dashboard-slot-per-month.tsx
+++ app/(dashboard)/dashboard/_components/dashboard-slot-per-month.tsx
@@ -1,6 +1,6 @@
 import DashboardSlotAreaChart from "./dashboard-slot-area-chart";
-import { chartData } from "../_lib/constant";
 import { getLastSixMonthsAppointments } from "../service";
+import { processReservationsData, Reservation } from "../_lib";
 
 const DashboardSlotPerMonth = async () => {
     const { data, error } = await getLastSixMonthsAppointments();
@@ -9,7 +9,7 @@ const DashboardSlotPerMonth = async () => {
         return <h2>Error par</h2>;
     }
 
-    console.log("data: ", data)
+    const chartData = processReservationsData(data as Reservation[]);
 
     return (
         <div className="flex flex-col gap-1 p-4 rounded-md bg-slate-100 dark:bg-slate-950 row-span-2 md:col-span-2">
