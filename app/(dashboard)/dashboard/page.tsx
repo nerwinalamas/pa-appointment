20c5a64eaeda7bb949694152f4286e18c3a76098
nerwinalamas <nerwinalamas@gmail.com>
2024-10-21 11:09:39 +0800

    refactor: dashboard page

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,32 +1,25 @@
 import DashboardBookings from "./_components/dashboard-bookings";
 import DashboardAvailableSlots from "./_components/dashboard-available-slots";
-import DashboardTotalSlots from "./_components/dashboard-total-slots";
-import DashboardTotalSales from "./_components/dashboard-total-sales";
-import DashboardSlotAreaChart from "./_components/dashboard-slot-area-chart";
+import DashboardTotalStaff from "./_components/dashboard-total-staff";
+import DashboardTotalDeposit from "./_components/dashboard-total-deposit";
+import DashboardSlotPerMonth from "./_components/dashboard-slot-per-month";
 import DashboardTable from "./_components/dashboard-table";
-import DashboardTotalBookingsChart from "./_components/dashboard-total-bookings-chart";
-import { getAllTotalBookings } from "./service";
+import DashboardTotalBookings from "./_components/dashboard-total-bookings";
 
-const Dashboard = async ({
+const Dashboard = ({
     searchParams,
 }: {
     searchParams: { page: string };
 }) => {
-    const { count, error } = await getAllTotalBookings();
-    
-    if (error) {
-        return <h2>Error par</h2>;
-    }
-
     return (
         <div className="p-4 grid gap-4 grid-rows-10 md:grid-rows-8 md:grid-cols-2 xl:grid-rows-4 xl:grid-cols-4">
             <DashboardBookings />
             <DashboardAvailableSlots />
-            <DashboardTotalSlots />
-            <DashboardTotalSales />
-            <DashboardSlotAreaChart />
+            <DashboardTotalStaff />
+            <DashboardTotalDeposit />
+            <DashboardSlotPerMonth />
             <DashboardTable searchParamsPage={searchParams.page} />
-            <DashboardTotalBookingsChart count={count as number} />
+            <DashboardTotalBookings />
         </div>
     );
 };
