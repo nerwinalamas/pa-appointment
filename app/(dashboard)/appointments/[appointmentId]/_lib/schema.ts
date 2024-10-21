1da5c70f131083f733ee193e12c401229be0b446
nerwinalamas <nerwinalamas@gmail.com>
2024-10-21 11:38:33 +0800

    refactor: remove unused code

--- app/(dashboard)/appointments/[appointmentId]/_lib/schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const slotSchema = z.object({
-    startTime: z.string().min(1, "Start time is required"),
-    endTime: z.string().min(1, "End time is required"),
-});
