d72ab9a0f37a6695b5ecd1652a628c83c98670d0
nerwinalamas <nerwinalamas@gmail.com>
2024-10-21 11:50:15 +0800

    refactor: remove unused code

--- app/(dashboard)/appointments/action.ts
+++ app/(dashboard)/appointments/action.ts
@@ -3,51 +3,6 @@
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 
-export const checkDateIfExists = async (date: string) => {
-    const supabase = await createClient();
-
-    const { data: existingAppointment, error: checkError } = await supabase
-        .from("appointments")
-        .select("*")
-        .eq("date", date)
-        .single();
-
-    if (checkError && checkError.code !== "PGRST116") {
-        return { exists: true, error: checkError.message };
-    }
-
-    if (existingAppointment) {
-        return {
-            exists: true,
-            error: "An appointment for this date already exists",
-        };
-    }
-
-    return { exists: false };
-};
-
-export const createAppointment = async (formData: FormData) => {
-    const date = formData.get("date") as string;
-
-    const existenceCheck = await checkDateIfExists(date);
-
-    if (existenceCheck.exists) {
-        return { success: false, error: existenceCheck.error };
-    }
-
-    const supabase = await createClient();
-    const { data, error } = await supabase.from("appointments").insert({
-        date,
-    });
-
-    if (error) {
-        return { success: false, error: error.message };
-    }
-
-    revalidatePath("/appointments");
-    return { success: true, data };
-};
-
 export const updateAppointment = async (
     appointmentId: string,
     formData: FormData
--- app/(dashboard)/appointments/service.ts
+++ app/(dashboard)/appointments/service.ts
@@ -1,24 +1,5 @@
 import { createClient } from "@/utils/supabase/server";
 
-export const getAllAppointmentsDashboard = async (page = 1, pageSize = 10) => {
-    const supabase = createClient();
-
-    const start = (page - 1) * pageSize;
-    const end = start + pageSize - 1;
-
-    const { data, error, count } = await supabase
-        .from("appointments")
-        .select("*", { count: "exact" })
-        .range(start, end)
-        .order("date", { ascending: true });
-
-    if (error) {
-        return { success: false, error: error.message };
-    }
-
-    return { success: true, data, count, page, pageSize };
-};
-
 export const getAllReservations = async (page = 1, pageSize = 10) => {
     const supabase = createClient();
 
