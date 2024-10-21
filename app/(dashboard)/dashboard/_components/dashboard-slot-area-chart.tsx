d7f128c852b27b55efc20fdd58a783d509e8d79f
nerwinalamas <nerwinalamas@gmail.com>
2024-10-21 14:10:19 +0800

    refactor: dashboard slot area chart

--- app/(dashboard)/dashboard/_components/dashboard-slot-area-chart.tsx
+++ app/(dashboard)/dashboard/_components/dashboard-slot-area-chart.tsx
@@ -1,61 +1,66 @@
 "use client";
 
 import { chartConfig } from "../_lib/chartConfig";
-
-import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+import { TARGET_BOOKINGS } from "../_lib/constant";
 import {
     ChartContainer,
     ChartTooltip,
     ChartTooltipContent,
 } from "@/components/ui/chart";
+import {
+    Area,
+    AreaChart,
+    CartesianGrid,
+    ResponsiveContainer,
+    XAxis,
+    YAxis,
+} from "recharts";
 
 type ChartData = {
     month: string;
     booking: number;
-    availableSlots: number;
 };
 
 const DashboardSlotAreaChart = ({ chartData }: { chartData: ChartData[] }) => {
     return (
-        <ChartContainer config={chartConfig} className="h-full">
-            <AreaChart
-                accessibilityLayer
-                data={chartData}
-                margin={{
-                    left: 12,
-                    right: 12,
-                }}
-            >
-                <CartesianGrid vertical={false} />
-                <XAxis
-                    dataKey="month"
-                    tickLine={false}
-                    axisLine={false}
-                    tickMargin={8}
-                    tickFormatter={(value) => value.slice(0, 3)}
-                />
-                <ChartTooltip
-                    cursor={false}
-                    content={<ChartTooltipContent indicator="dot" />}
-                />
-                <Area
-                    dataKey="availableSlots"
-                    type="natural"
-                    fill="var(--color-availableSlots)"
-                    fillOpacity={0.4}
-                    stroke="var(--color-availableSlots)"
-                    stackId="a"
-                />
-                <Area
-                    dataKey="booking"
-                    type="natural"
-                    fill="var(--color-booking)"
-                    fillOpacity={0.4}
-                    stroke="var(--color-booking)"
-                    stackId="a"
-                />
-            </AreaChart>
-        </ChartContainer>
+        <ResponsiveContainer width="100%" height={300}>
+            <ChartContainer config={chartConfig}>
+                <AreaChart
+                    accessibilityLayer
+                    data={chartData}
+                    margin={{
+                        left: 12,
+                        right: 12,
+                    }}
+                >
+                    <CartesianGrid vertical={false} />
+                    <XAxis
+                        dataKey="month"
+                        tickLine={false}
+                        axisLine={false}
+                        tickMargin={8}
+                        tickFormatter={(value) => value.slice(0, 3)}
+                    />
+                    <YAxis
+                        axisLine={false}
+                        tickLine={false}
+                        tickFormatter={(value) => `${value}`}
+                        domain={[0, TARGET_BOOKINGS]}
+                    />
+                    <ChartTooltip
+                        cursor={false}
+                        content={<ChartTooltipContent indicator="line" />}
+                    />
+                    <Area
+                        dataKey="booking"
+                        type="natural"
+                        fill="var(--color-booking)"
+                        fillOpacity={0.4}
+                        stroke="var(--color-booking)"
+                    />
+                </AreaChart>
+            </ChartContainer>
+        </ResponsiveContainer>
     );
 };
 
