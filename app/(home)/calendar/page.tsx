2067ad930828324a9061a1967ca5ad91399f85f4
nerwinalamas <nerwinalamas@gmail.com>
2024-10-21 14:11:42 +0800

    refactor: calendar page

--- app/(home)/calendar/page.tsx
+++ app/(home)/calendar/page.tsx
@@ -1,9 +1,26 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 import CalendarDayPicker from "./_components/calendar-day-picker";
 
 const AppointmentCalendar = () => {
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <CalendarDayPicker />
+            <Card>
+                <CardHeader>
+                    <CardTitle>Select a Date</CardTitle>
+                    <CardDescription>
+                        Choose a date for your appointment
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <CalendarDayPicker />
+                </CardContent>
+            </Card>
         </div>
     );
 };
