8931e789f673c43921bf72bc3aad1b1f2f87e4df
nerwinalamas <nerwinalamas@gmail.com>
2024-10-23 16:11:50 +0800

    feat: calendar constant

---
+++ app/(home)/calendar/_lib/constant.ts
@@ -0,0 +1,24 @@
+export const DAY_NAMES = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+export const MONTH_NAMES = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
