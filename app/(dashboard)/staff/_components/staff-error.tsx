6b7502bbaac1490a9fe5ef154594ba5411e975ec
nerwinalamas <nerwinalamas@gmail.com>
2024-10-24 12:45:58 +0800

    feat: staff error

---
+++ app/(dashboard)/staff/_components/staff-error.tsx
@@ -0,0 +1,12 @@
+import { AlertCircle } from "lucide-react";
+
+const StaffError = () => {
+    return (
+        <div className="flex flex-col items-center min-h-screen xl:min-h-96 justify-center gap-1 pt-5 pb-20 lg:pb-12 lg:gap-0 lg:mx-auto xl:m-4 xl:p-4 xl:pb-10 bg-slate-100 dark:bg-slate-950">
+            <AlertCircle className="w-12 h-12 text-red-500" />
+            <h2 className="text-base font-light">Error in Staff Table</h2>
+        </div>
+    );
+};
+
+export default StaffError;
